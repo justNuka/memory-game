848883b93f6d575a6e418bf1978e6f2f3bd86fac
elias <elias59410@gmail.com>
2023-12-08 08:58:33 +0100

    affichage

--- memories.js
+++ memories.js
@@ -1,54 +1,65 @@
-const cards = [
-    'A', 'A', 'B', 'B',
-    'C', 'C', 'D', 'D',
-    'E', 'E', 'F', 'F',
-    'G', 'G', 'H', 'H'
-];
+const cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  let flippedCards = [];
+  let matchedCards = [];
 
-// Mélanger les cartes
-cards.sort(() => Math.random() - 0.5);
+  function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
 
-// Sélectionner l'élément du jeu
-const memoryGame = document.getElementById('memory-game');
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-// Initialiser le tableau pour suivre les cartes retournées
-let flippedCards = [];
-
-// Créer les cartes et les ajouter à la page
-cards.forEach((card, index) => {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('memory-card');
-    cardElement.dataset.card = card;
-    cardElement.dataset.index = index;
-    cardElement.addEventListener('click', flipCard);
-    memoryGame.appendChild(cardElement);
-});
-
-// Fonction pour retourner une carte
-function flipCard() {
-    const selectedCard = this;
-    selectedCard.textContent = selectedCard.dataset.card;
-
-    flippedCards.push(selectedCard);
-
-    if (flippedCards.length === 2) {
-        setTimeout(checkMatch, 500);
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-}
 
-// Fonction pour vérifier si les cartes retournées forment une paire
-function checkMatch() {
+    return array;
+  }
+
+  function createCard(cardValue) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.textContent = cardValue;
+    card.addEventListener('click', () => flipCard(card));
+    return card;
+  }
+
+  function flipCard(card) {
+    if (flippedCards.length < 2 && !flippedCards.includes(card) && !matchedCards.includes(card)) {
+      card.classList.add('flipped');
+      flippedCards.push(card);
+
+      if (flippedCards.length === 2) {
+        setTimeout(checkMatch, 1000);
+      }
+    }
+  }
+
+  function checkMatch() {
     const [card1, card2] = flippedCards;
+    const value1 = card1.textContent;
+    const value2 = card2.textContent;
 
-    if (card1.dataset.card === card2.dataset.card) {
-        // Paire trouvée
-        card1.removeEventListener('click', flipCard);
-        card2.removeEventListener('click', flipCard);
+    if (value1 === value2) {
+      matchedCards.push(card1, card2);
+      flippedCards = [];
+
+      if (matchedCards.length === cards.length) {
+        alert('Félicitations, vous avez gagné !');
+      }
     } else {
-        // Pas de correspondance, retourner les cartes face cachée
-        card1.textContent = '';
-        card2.textContent = '';
+      flippedCards.forEach(card => card.classList.remove('flipped'));
+      flippedCards = [];
     }
+  }
 
-    flippedCards = [];
-}
+  function initGame() {
+    const shuffledCards = shuffle(cards);
+    const gameContainer = document.getElementById('memory-game');
+
+    shuffledCards.forEach(cardValue => {
+      const card = createCard(cardValue);
+      gameContainer.appendChild(card);
+    });
+  }
+
+  initGame();
