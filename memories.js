b6b7b02b5dfc0f032672cac32aa96b7e0edee966
mlenoir4 <mlenoir4@myges.fr>
2023-12-08 10:15:28 +0100

    slt

--- memories.js
+++ memories.js
@@ -88,3 +88,25 @@ const cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 
       gameContainer.appendChild(card);
     });
   }
+
+
+  describe('Pagination Unit Test Suites', () => {
+    it('should return something', () => (
+        expect(Pagination.getNumberOfPages(12)).toBeDefined()
+    ))
+
+ 
+    it('should return 0', () => (
+        expect(Pagination.getNumberOfPages(0)).toEqual(0)
+    ))
+
+ 
+    it('should return 1', () => (
+        expect(Pagination.getNumberOfPages(7)).toEqual(1)
+    ))
+
+ 
+    it('should return 5', () => (
+        expect(Pagination.getNumberOfPages(34)).toEqual(5)
+    ))
+})
